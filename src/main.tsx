10a01bb366e8b1414bce3c1f75a82c9deb31329f
Giulex84 <giulianom.gm@gmail.com>
2026-01-12 01:40:54 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const container = document.getElementById("root");
-
-if (!container) {
-  throw new Error("Root container missing");
-}
-
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <App />
+);
