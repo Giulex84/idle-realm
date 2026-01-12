cf7c348ed25b307d9df71f8946a2e5e6b3c1f713
Giulex84 <giulianom.gm@gmail.com>
2026-01-12 01:06:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",   // ⬅️ FONDAMENTALE
+  base: "/",
+  publicDir: "public", // forzato esplicitamente
   plugins: [react()],
+  build: {
+    copyPublicDir: true, // ⬅️ CHIAVE
+  },
 });
